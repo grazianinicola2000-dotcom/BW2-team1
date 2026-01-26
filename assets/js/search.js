20bb3fba14ddff01f2c094223cd91f3051977d40
EmeraldNV <abdellah316619@gmail.com>
2026-01-26 16:34:59 +0100

    Refactor cardColorsGenerator function for improved readability and consistency

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,26 +1,66 @@
+function cardColorsGenerator() {
+  const cardColors = [
+    "#e13300",
+    "#1e3264",
+    "#e8125c",
+    "#158a08",
+    "#bc5800",
+    "#7a5a95",
+    "#503750",
+    "#2d46b9",
+    "#777777",
+    "#8c1932",
+    "#a56752",
+    "#7d4b32",
+  ];
 
-function cardColorsGenerator (){
-const cardColors = ["#e13300", "#1e3264", "#e8125c", "#158a08", "#bc5800", "#7a5a95", "#503750", "#2d46b9", "#777777", "#8c1932", "#a56752", "#7d4b32"];
+  const randomIndex = () => {
+    return Math.floor(Math.random() * cardColors.length);
+  };
 
-const randomIndex = () => {
-  return Math.floor(Math.random() * cardColors.length);
-};
+  const cards = document.querySelectorAll(".cardGenresNav");
 
-const cards = document.querySelectorAll(".cardGenresNav");
-
-cards.forEach((card) => {
-  card.style.backgroundColor = cardColors[randomIndex()];
-});
+  cards.forEach((card) => {
+    card.style.backgroundColor = cardColors[randomIndex()];
+  });
 }
 
-function getData(){
-  
+async function tokenSearch() {
+  const searchBar = document.querySelector("input");
+  const searchButton = document.querySelector(".fa-solid.fa-magnifying-glass.m-0.p-0.fs-1");
+
+  searchButton.addEventListener("click", (e) => {
+    let query = "";
+    let urlAPI = `https://striveschool-api.herokuapp.com/api/deezer/search?q=`;
+
+    e.preventDefault();
+    if (searchBar.value.toLowerCase() == "") {
+      alert("inserisci un'artista o un brano");
+    } else {
+      query = searchBar.value;
+
+      urlAPI = urlAPI + query;
+      console.log(urlAPI);
+      getData(urlAPI);
+    }
+  });
 }
 
+async function getData(searchAPI) {
+  try {
+    const response = await fetch(searchAPI);
+    if (!response.ok) {
+      throw new Error("Errore");
+    }
+    const data = response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Errore", error);
+  }
+}
 
 window.onload = () => {
-
-cardColorsGenerator();
-
-
-}
+  tokenSearch();
+  cardColorsGenerator();
+};
