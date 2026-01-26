5c57cf0cfe9694961cae43b3b60a08b056e49886
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-01-26 21:32:48 +0100

    .

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,11 +1,6 @@
-const cardColors = ["#e13300", "#1e3264", "#e8125c", "#158a08", "#bc5800", "#7a5a95", "#503750", "#2d46b9", "#777777", "#8c1932", "#a56752", "#7d4b32"];
 let query = "";
 
-const randomIndex = () => {
-  return Math.floor(Math.random() * cardColors.length);
-};
 
-const cards = document.querySelectorAll(".cardGenresNav");
 function cardColorsGenerator() {
   const cardColors = [
     "#e13300",
@@ -49,9 +44,6 @@ async function tokenSearch() {
       urlAPI = urlAPI + query;
       const dataToken = await getData(urlAPI);
 
-cards.forEach((card) => {
-  card.style.backgroundColor = cardColors[randomIndex()];
-});
       populateCard(dataToken.data);
       generateSongsCard(dataToken.data);
       ///
