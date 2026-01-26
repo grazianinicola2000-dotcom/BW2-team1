8a66d6da24c4c6191afb65756a700e1d0b614ba0
EmeraldNV <abdellah316619@gmail.com>
2026-01-26 15:26:40 +0100

    Update search.js and homepage.html with function definition and localization changes

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,5 @@
+
+function cardColorsGenerator (){
 const cardColors = ["#e13300", "#1e3264", "#e8125c", "#158a08", "#bc5800", "#7a5a95", "#503750", "#2d46b9", "#777777", "#8c1932", "#a56752", "#7d4b32"];
 
 const randomIndex = () => {
@@ -9,3 +11,16 @@ const cards = document.querySelectorAll(".cardGenresNav");
 cards.forEach((card) => {
   card.style.backgroundColor = cardColors[randomIndex()];
 });
+}
+
+function getData(){
+  
+}
+
+
+window.onload = () => {
+
+cardColorsGenerator();
+
+
+}
