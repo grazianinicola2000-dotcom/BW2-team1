518fb08e781eb11a85210a6882a132ce934907fa
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-01-26 13:53:18 +0100

    search.html + search.js and search.scss without sidebar

---
+++ assets/js/search.js
@@ -0,0 +1,11 @@
+const cardColors = ["#e13300", "#1e3264", "#e8125c", "#158a08", "#bc5800", "#7a5a95", "#503750", "#2d46b9", "#777777", "#8c1932", "#a56752", "#7d4b32"];
+
+const randomIndex = () => {
+  return Math.floor(Math.random() * cardColors.length);
+};
+
+const cards = document.querySelectorAll(".cardGenresNav");
+
+cards.forEach((card) => {
+  card.style.backgroundColor = cardColors[randomIndex()];
+});
