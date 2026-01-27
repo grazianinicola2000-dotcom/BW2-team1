159e8e4b71984a89c996cf588bfe16140299ce0f
EmeraldNV <abdellah316619@gmail.com>
2026-01-27 09:47:07 +0100

    Add explicit content indicator to song cards in generateSongsCard function

--- assets/js/search.js
+++ assets/js/search.js
@@ -55,6 +55,7 @@ async function generateSongsCard(data) {
   const songsContainer = document.querySelector("#songsContainer");
 songsContainer.innerHTML = '<h3>Songs</h3>'
   for (let i = 0; i < 4; i++) {
+    
     let duration = await secondsToMinutes(data[i]);
     songsContainer.innerHTML += `
   <div class="d-flex p-2 rounded-2 searcheSongsContainer">
@@ -62,16 +63,21 @@ songsContainer.innerHTML = '<h3>Songs</h3>'
                 <div class="d-flex justify-content-between flex-grow-1 align-items-center">
                   <div>
                     <h4 class="m-0">${data[i].title}</h4>
-                    <div class="d-flex">
-                      <i class="bi bi-explicit-fill"></i>
-                      <p class="m-0 p-0">${data[i].artist.name}</p>
+                    <div class="d-flex explicit${i}">
+                      
+                      
                     </div>
                   </div>
                   <p class="m-0">${duration}</p>
                 </div>
               </div>
               `;
+              let explicit = document.querySelector('.explicit'+ i)
+              data[i].explicit_content_lyrics == 1 ? explicit.innerHTML = `<i class="bi bi-explicit-fill"></i><p class="m-0 p-0">${data[i].artist.name}</p>` : `<p class="m-0 p-0">${data[i].artist.name}</p>`
+              
   }
+  
+
 }
 
 async function secondsToMinutes(data) {
