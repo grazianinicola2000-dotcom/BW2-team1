bcb32dcbc10c5038ecd5e5c61bf53e2113daf839
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-01-30 15:03:51 +0100

    feat: add search.css for improved styling and refactor HTML structure for better readability

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -473,15 +473,12 @@ btn.onclick = async (e) => {
 
 function redirectArtist() {
   const artistElements = document.querySelectorAll(".artistElement");
-  const idElements = document.querySelectorAll(".song");
 
   artistElements.forEach((element, index) => {
     element.onclick = (e) => {
       e.preventDefault();
 
-      const correctId = idElements[index].innerText;
-      window.location.href = `./artist.html?id=${correctId}`;
-      console.log(correctId);
+      window.location.href = `./artist.html?id=${localStorage.getItem("artistId")}`;
     };
   });
 }
